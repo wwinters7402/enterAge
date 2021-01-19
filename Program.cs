d41b3f9bd9db022479742f72031fb7e7f3adfab2
woodrowdeveloper <woodrowdeveloper@gmail.com>
2021-01-19 16:40:59 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace enterAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Enter Your Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            while(age < 18)
+            {
+
+                Console.WriteLine("Keep entering an age. You have not reached the legal age yet. ");
+                age = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("You have reached the legal age.");
+           
+
+           
+            
+            
+
+                
+            
+            
+           
+                    
+
+
+            
+             
+
+            }
+        }
+    }
+
